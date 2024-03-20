badd5d5397c857eb5d8d8504b8a78523bf5f4968
Maksim M0rokov <maxmorokov7@gmail.com>
2024-03-20 18:38:54 +0300

    v1.2/1/22

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -7,16 +7,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.model.Role;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.kata.spring.boot_security.demo.model.User;
-import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
 import ru.kata.spring.boot_security.demo.services.RoleService;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @RequestMapping("/admin")
 @Controller
@@ -25,34 +24,28 @@ public class AdminController {
     private final UserService userService;
 
 
-    private final RoleService roleRepository;
-
-    @Autowired
-    public AdminController(UserService userService, RoleService roleRepository) {
+    @Autowired()
+    public AdminController(UserService userService ) {
         this.userService = userService;
-        this.roleRepository = roleRepository;
+
     }
 
     @GetMapping("/")
-    public String table(Model model, @CurrentSecurityContext(expression = "authentication.principal") User principal) {
+    public String table(Model model,@CurrentSecurityContext(expression = "authentication.principal") User principal) {
         model.addAttribute("table", userService.getAllUsers());
         model.addAttribute("user", principal);
         return "table";
     }
 
+
     @PostMapping("/new")
-    public String add(@ModelAttribute("user") User user, BindingResult bindingResult,
-                      @RequestParam("role") List<String> roleNames) {
-        if (!bindingResult.hasErrors()) {
-            List<Role> roles = new ArrayList<>();
-            for (String roleName : roleNames) {
-                Optional<Role> byId = roleRepository.findRoleByName(roleName);
-                byId.ifPresent(roles::add);
-            }
-            user.setRoles(roles);
+    public String add(@ModelAttribute("user") User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "create";
+        } else {
             userService.addUser(user);
+            return "redirect:/admin/";
         }
-        return "redirect:/admin/";
     }
 
     @PostMapping("/delete")
@@ -70,4 +63,6 @@ public class AdminController {
         }
         return "redirect:/admin/";
     }
+
+
 }
